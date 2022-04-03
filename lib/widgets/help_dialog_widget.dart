21160596a290164917ae2b52355fcfc5efe5548a
maximus <maximusagub@gmail.com>
2022-04-02 21:10:12 -0700

    working on the onboarding screen

--- lib/widgets/help_dialog_widget.dart
+++ lib/widgets/help_dialog_widget.dart
@@ -1,10 +1,13 @@
 import 'dart:io';
+import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:kingsfam/extensions/hexcolor.dart';
 
 Future<void> helpDialog(BuildContext context) async {
   return showDialog(context: context, builder: (context) {
     return AlertDialog(
+      backgroundColor: Colors.grey[900] ,
       content: howToBox(),
     );
   }
@@ -12,17 +15,20 @@ Future<void> helpDialog(BuildContext context) async {
 }
 
 Widget howToBox() {
+  int currentIndex = 0;
   return StatefulBuilder(
     builder: (BuildContext context, setState) {
       return Container(
         decoration: BoxDecoration(borderRadius: BorderRadius.circular(25), color: Colors.grey[900]),
-        height: 300,
+        height: 400,
+        width: 450,
         child: PageView.builder(
           itemCount: 3,
+          onPageChanged: (int index) {setState(() {currentIndex = index;}); },
           itemBuilder: (_, i) {
             return Padding(
-              padding: EdgeInsets.symmetric(horizontal: 40, vertical: 40),
-              child: Text("Step 1."),
+              padding: EdgeInsets.symmetric(horizontal: 10, vertical: 35),
+              child: _contents(currentIndex)[i],
             );
           }
         ),
@@ -38,10 +44,108 @@ class onboardingContent {
   onboardingContent(this.image, this.text);
 }
 
-List<Widget> _contents() {
+List<Widget> _contents(int currIdx) {
+  HexColor hexcolor = HexColor();
   return   [
-    Container(),
-    Container(),
-    Container(),
+    Column(
+      children: [
+        Text(
+          " Hey Fam, Find A Commuinity or ... Create One! \n"
+          " Talk About Whatever But Keep Jesus At the Center! \n"
+          " Yeherdd! ",
+          textAlign: TextAlign.center,
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+
+
+        SizedBox(height: 35),
+
+        Container(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: List.generate(3, (index) => buildDot(index, currIdx, hexcolor) ),
+          ),
+        ),
+
+
+        SizedBox(height: 30),
+
+
+        ElevatedButton(
+          onPressed: () {}, 
+          child: Text("Next"),
+          style: ElevatedButton.styleFrom(primary: Color(hexcolor.hexcolorCode('#FFC050'))),
+        )
+      ],
+    ),
+
+    // --------------------------------> child 2
+    Column(
+      children: [
+        Text(
+          " Hey Fam, Find A Commuinity You Want To Join! "
+          " Talk About Whatever But Keep Jesus At the Center! \n"
+          " Yeherdd! ",
+          textAlign: TextAlign.center,
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+
+        SizedBox(height: 35),
+
+
+        Container(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: List.generate(3, (index) => buildDot(index, currIdx, hexcolor) ),
+          ),
+        ),
+
+
+        SizedBox(height: 30),
+
+
+        ElevatedButton(
+          onPressed: () {}, 
+          child: Text("Next"),
+          style: ElevatedButton.styleFrom(primary: Color(hexcolor.hexcolorCode('#FFC050'))),
+        )
+      ],
+    ),
+    // ----------------------------------------------> child 3
+    Column(
+      children: [
+        Text(
+          " Hey Fam, Find A Commuinity You Want To Join! "
+          " Talk About Whatever But Keep Jesus At the Center! \n"
+          " Yeherdd! ",
+          textAlign: TextAlign.center,
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+
+        SizedBox(height: 35),
+
+
+        Container(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: List.generate(3, (index) => buildDot(index, currIdx, hexcolor) ),
+          ),
+        ),
+
+        SizedBox(height: 30),
+
+        ElevatedButton(
+          onPressed: () {}, 
+          child: Text("Lets Go!"),
+          style: ElevatedButton.styleFrom(primary: Color(hexcolor.hexcolorCode('#FFC050'))),
+        )
+      ],
+    )
   ];
+}
+
+Container buildDot(int index, int currIdx, HexColor hexcolor) {
+  return Container(
+    height: 10, width: currIdx == index ? 18 : 10, margin: EdgeInsets.only(right: 5), decoration: BoxDecoration(borderRadius: BorderRadius.circular(25), color: Color(hexcolor.hexcolorCode('#FFC050'))),
+  );
 }
