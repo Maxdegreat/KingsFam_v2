9aff2b50c47660c894d654e2a31cacbaa4eaace1
Maximus <max@Maximuss-MacBook-Air.local>
2022-07-20 23:47:09 -0700

    ios config

--- lib/helpers/ad_helper.dart
+++ lib/helpers/ad_helper.dart
@@ -6,31 +6,31 @@ class AdHelper {
     if (Platform.isAndroid) {
       return "ca-app-pub-3940256099942544/6300978111";
     } else if (Platform.isIOS) {
-      return "ca-app-pub-3940256099942544/6300978111";
+      return "ca-app-pub-3940256099942544/2934735716";
     } else {
       throw new UnsupportedError("Unsupported platform");
     }
   }
 
-  static String get nativeAdId {
-    if (Platform.isAndroid) {
-      return 'ca-app-pub-3940256099942544/2247696110';
-    } else if (Platform.isIOS) {
-      return 'ca-app-pub-3940256099942544/2247696110';
-    } else {
-      throw new UnsupportedError("Unsupported platform");
-    }
-  }
+  // static String get nativeAdId {
+  //   if (Platform.isAndroid) {
+  //     return 'ca-app-pub-3940256099942544/2247696110';
+  //   } else if (Platform.isIOS) {
+  //     return 'ca-app-pub-3940256099942544/2247696110';
+  //   } else {
+  //     throw new UnsupportedError("Unsupported platform");
+  //   }
+  // }
 
-  static String get getinsteritalAdUnitId {
-    if (Platform.isAndroid) {
-      return 'ca-app-pub-3940256099942544/1033173712';
-    } else if (Platform.isIOS) {
-      return 'ca-app-pub-3940256099942544/1033173712';
-    } else {
-      throw new UnsupportedError("Unsupported platform");
-    }
-  }
+  // static String get getinsteritalAdUnitId {
+  //   if (Platform.isAndroid) {
+  //     return 'ca-app-pub-3940256099942544/1033173712';
+  //   } else if (Platform.isIOS) {
+  //     return 'ca-app-pub-3940256099942544/1033173712';
+  //   } else {
+  //     throw new UnsupportedError("Unsupported platform");
+  //   }
+  // }
 
 }
 
