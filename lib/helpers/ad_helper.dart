edf00b631aa4bef0f5ac702cacf9c4fcc9e44bde
maximus <maximusagub@gmail.com>
2022-09-19 04:56:50 -0700

    switched to production ID  for android admob

--- lib/helpers/ad_helper.dart
+++ lib/helpers/ad_helper.dart
@@ -7,7 +7,7 @@ class AdHelper {
     String testAdUnitId = "ca-app-pub-3940256099942544/6300978111";
     if (Platform.isAndroid) {
       // ignore: dead_code
-      return testAdUnitId ;
+      return prodAdUnitIdA ;
     } else if (Platform.isIOS) {
       return "!ca-app-pub-3940256099942544/2934735716";
     } else {
