f175ea2b4019e51f3bde9120385a6c718341ae24
maximus <maximusagub@gmail.com>
2023-01-23 13:42:56 -0600

    making kf pofessional now

--- lib/widgets/comment_line.dart
+++ lib/widgets/comment_line.dart
@@ -1,10 +1,16 @@
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:kingsfam/config/paths.dart';
+import 'package:kingsfam/cubits/buid_cubit/buid_cubit.dart';
 
 import 'package:kingsfam/extensions/hexcolor.dart';
 
 import 'package:kingsfam/screens/profile/profile_post_view.dart';
 import 'package:kingsfam/screens/profile/profile_screen.dart';
+import 'package:kingsfam/screens/report_content_screen.dart';
+import 'package:kingsfam/widgets/hide_content/hide_content_full_screen_post.dart';
 
 import '../models/comment_model.dart';
 
@@ -28,7 +34,13 @@ class _CommentLinesState extends State<CommentLines> {
   _commentLine() {
     return Padding(
       padding: const EdgeInsets.all(8.0),
-      child: Column(
+      child: 
+      
+      context.read<BuidCubit>().state.buids.contains(widget.comment.author.id) 
+
+      ? HideContent.textContent(Theme.of(context).textTheme, () {context.read<BuidCubit>().onBlockUser(widget.comment.author.id); Navigator.of(context).pop();})
+
+      : Column(
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
@@ -43,7 +55,15 @@ class _CommentLinesState extends State<CommentLines> {
   _header() {
     return GestureDetector(
       onTap: () {
-        Navigator.of(context).pushNamed(ProfilePostView.routeName, arguments: ProfileScreenArgs(userId: widget.comment.author.id, initScreen: true));
+        Navigator.of(context).pushNamed(ProfileScreen.routeName, arguments: ProfileScreenArgs(userId: widget.comment.author.id, initScreen: true));
+      },
+      onLongPress: () {
+         Map<String, dynamic> info = {
+          "userId" : widget.comment.author.id,
+          "what" : "comment",
+          "continue": FirebaseFirestore.instance.collection(Paths.posts).doc(widget.comment.postId).collection(Paths.comments).doc(widget.comment.id),                 
+        };
+        Navigator.of(context).pushNamed(ReportContentScreen.routeName, arguments: RepoetContentScreenArgs(info: info));
       },
       child: Row(
         mainAxisAlignment: MainAxisAlignment.start,
