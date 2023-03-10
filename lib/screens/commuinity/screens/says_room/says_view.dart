3fc6bd2b1c2c4561298d8f4465851429d8f25d82
maximus <maximusagub@gmail.com>
2023-03-10 16:13:09 -0600

    updated the says

--- lib/screens/commuinity/screens/says_room/says_view.dart
+++ lib/screens/commuinity/screens/says_room/says_view.dart
@@ -101,50 +101,50 @@ class _SaysViewState extends State<SaysView> {
               mainAxisAlignment: MainAxisAlignment.start,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    GestureDetector(
-                      onTap: () {
-                        if (widget.s.id != null) {
-                          context
-                              .read<LikedSaysCubit>()
-                              .updateOnCloudLike(
-                                  cmId: widget.cmId,
-                                  kcId: widget.kcId,
-                                  sayId: widget.s.id!,
-                                  currLikes: widget.s.likes)
-                              .then((value) => setState(() {}));
-                        } else
-                          snackBar(
-                              snackMessage: "s.id is null",
-                              context: context,
-                              bgColor: Colors.red);
-                      },
-                      child: Icon(
-                        Icons.favorite_border_outlined,
-                        size: 25,
-                        color: hasLiked
-                            ? Colors.amber
-                            : Theme.of(context).iconTheme.color,
-                      ),
-                    ),
-                    SizedBox(width: 8),
-                    Text(
-                        hasLiked
-                            ? (widget.s.likes + 1).toString()
-                            : widget.s.likes.toString(),
-                        style: Theme.of(context).textTheme.caption),
-                    // SizedBox(width: 7),
-                    // Icon(Icons.mode_comment_outlined),
-                    // SizedBox(width: 1),
-                    // Text(widget.s.commentsCount.toString(),
-                    //     style: Theme.of(context).textTheme.caption),
-                    // SizedBox(width: 7),
-                  ],
-                ),
-                SizedBox(height: 8),
+                // Row(
+                //   mainAxisAlignment: MainAxisAlignment.start,
+                //   crossAxisAlignment: CrossAxisAlignment.center,
+                //   children: [
+                //     GestureDetector(
+                //       onTap: () {
+                //         if (widget.s.id != null) {
+                //           context
+                //               .read<LikedSaysCubit>()
+                //               .updateOnCloudLike(
+                //                   cmId: widget.cmId,
+                //                   kcId: widget.kcId,
+                //                   sayId: widget.s.id!,
+                //                   currLikes: widget.s.likes)
+                //               .then((value) => setState(() {}));
+                //         } else
+                //           snackBar(
+                //               snackMessage: "s.id is null",
+                //               context: context,
+                //               bgColor: Colors.red);
+                //       },
+                //       child: Icon(
+                //         Icons.favorite_border_outlined,
+                //         size: 25,
+                //         color: hasLiked
+                //             ? Colors.amber
+                //             : Theme.of(context).iconTheme.color,
+                //       ),
+                //     ),
+                //     SizedBox(width: 8),
+                //     Text(
+                //         hasLiked
+                //             ? (widget.s.likes + 1).toString()
+                //             : widget.s.likes.toString(),
+                //         style: Theme.of(context).textTheme.caption),
+                //     // SizedBox(width: 7),
+                //     // Icon(Icons.mode_comment_outlined),
+                //     // SizedBox(width: 1),
+                //     // Text(widget.s.commentsCount.toString(),
+                //     //     style: Theme.of(context).textTheme.caption),
+                //     // SizedBox(width: 7),
+                //   ],
+                // ),
+                // SizedBox(height: 8),
                 Padding(
                   padding: const EdgeInsets.only(left: 5.0),
                   child: Text(widget.s.date.timeAgo(),
